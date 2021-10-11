92ad4204c2c341ec8b6592dc89a00cf1161b339f
Evalina Kireilyte <evalinakireilyte@michaels-mbp-2.lan>
2021-10-11 16:26:09 +0100

    pacman-game

--- app.js
+++ app.js
@@ -6,33 +6,32 @@ function init() {
         1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
         1, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 1,
         1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1,
-        1, 0, 1, 1, 1, 0, 1, 2, 1, 0, 1, 1, 1, 0, 1,
-        1, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 1,
+        1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1,
+        1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1,
         1, 1, 0, 1, 1, 0, 1, 2, 1, 0, 1, 1, 0, 1, 1,
         1, 1, 0, 1, 1, 0, 1, 2, 1, 0, 1, 1, 0, 1, 1, 
         0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0,
         1, 1, 0, 1, 1, 0, 1, 2, 1, 0, 1, 1, 0, 1, 1,
         1, 1, 0, 1, 1, 0, 1, 2, 1, 0, 1, 1, 0, 1, 1, 
-        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+        1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1,
         1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1,
         1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1,
         1, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 1,
         1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 
-        // 0 - dot
+        // 0 - dot/fish
         // 1 - wall
         // 2 - ghost home
-        // 3 - fruit
+        // 3 - fruit/mice
         // 4 - empty 
     
     let grid = document.querySelector('.grid')
     const gridCells = []
 
-    const pacmanClass = 'pacman' // define the class of the character
-    let pacmanStartPosition = 112 // starting position of the pacman (refers to an index)
-    let pacmanCurrentPosition = 112 // use let to track where the pacman currently is (refers to an index)
-    let pacmanIndex = Array.from(grid).indexOf(gridCells)
-   
+    let pacmanCurrentPosition = 107 // use let to track where the pacman currently is (refers to an index)
+
+    let score = 0
+
 
     let ghost1StartPosition = 33
     let ghost2StartPosition = 57
@@ -55,7 +54,6 @@ function init() {
             grid.appendChild(cell)
             gridCells.push(cell)
             cell.classList.add('grid-cell')
-            console.log(cell)
 
          // add layout to the grid
 
@@ -75,97 +73,101 @@ function init() {
 // * Add pacman to grid
 
     function addPacman() {
-        gridCells[pacmanStartPosition].classList.add('pacmanClass')
-        console.log(pacmanStartPosition)
+        gridCells[pacmanCurrentPosition].classList.add('pacmanClass')
+        console.log(pacmanCurrentPosition)
     }
     addPacman() // call the function to add the pacman at its starting position
 
 // * Move pacman
 
-    const handleArrowUp = () => {
-        console.log('handleArrowUp')
-        pacmanCurrentPosition -= 15
-        move()
-    }
-    const handleArrowLeft = () => {
-        console.log('handleArrowLeft')
-        pacmanCurrentPosition -= 1
-        move()
-    }
-  
-    const handleArrowDown = () => {
-        console.log('handleArrowDown')
-        pacmanCurrentPosition += 15
-        move()
-    }
-    const handleArrowRight = () => {
-        console.log('handleArrowRight')
-        pacmanCurrentPosition += 1
-        move()
-    }
-  
-    function move() {
-        gridCells[pacmanStartPosition].classList.remove('pacmanClass')
-        gridCells[pacmanCurrentPosition].classList.add('pacmanClass')
-        pacmanStartPosition = pacmanCurrentPosition
-    }
-    move()
+    function movePacman() {
 
-// * Add ghost1 to grid
+        gridCells[pacmanCurrentPosition].classList.remove('pacmanClass')
 
-    function addGhost1(position) {
-        gridCells[position].classList.add('ghost1')
-        console.log(position)
-    }
-    addGhost1(ghost1StartPosition) 
-
-// * Add ghost2 to grid
-
-    function addGhost2(position) {
-        gridCells[position].classList.add('ghost2')
-        console.log(position)
-    }
-    addGhost2(ghost2StartPosition) 
-
-// * Add ghost3 to grid
-
-    function addGhost3(position) {
-        gridCells[position].classList.add('ghost3')
-        console.log(position)
-    }
-    addGhost3(ghost3StartPosition) 
-
-// * Add ghost4 to grid
-
-    function addGhost4(position) {
-        gridCells[position].classList.add('ghost4')
-        console.log(position)
-    }
-    addGhost4(ghost4StartPosition) 
-
-
-
-
-
-
-
-
-    document.addEventListener('keydown', function (event) {
         switch (event.key) {
-          case 'ArrowUp':
-            handleArrowUp()
-            break
-          case 'ArrowLeft':
-            handleArrowLeft()
-            break
-          case 'ArrowDown':
-            handleArrowDown()
-            break
-          case 'ArrowRight':
-            handleArrowRight()
-            break
+            case 'ArrowUp':
+                if (pacmanCurrentPosition - width >=0 && !gridCells[pacmanCurrentPosition-width].classList.contains('wall') && !gridCells[pacmanCurrentPosition-width].classList.contains('ghost-home')) {
+                    pacmanCurrentPosition -= width
+                }
+                break
+            case 'ArrowLeft':
+                if (pacmanCurrentPosition % width !==0 && !gridCells[pacmanCurrentPosition-1].classList.contains('wall') && !gridCells[pacmanCurrentPosition-1].classList.contains('ghost-home')) {
+                    pacmanCurrentPosition -= 1
+                }    
+                if ((pacmanCurrentPosition -1) === 104) {
+                    pacmanCurrentPosition = 119
+                }
+                break
+            case 'ArrowDown':
+                if (pacmanCurrentPosition + width < width * width && !gridCells[pacmanCurrentPosition+width].classList.contains('wall') && !gridCells[pacmanCurrentPosition+width].classList.contains('ghost-home')) {
+                    pacmanCurrentPosition += width
+                }
+                break
+            case 'ArrowRight':
+                if (pacmanCurrentPosition % width < width-1 && !gridCells[pacmanCurrentPosition+1].classList.contains('wall') && !gridCells[pacmanCurrentPosition+1].classList.contains('ghost-home')) {
+                    pacmanCurrentPosition += 1
+                }
+                if ((pacmanCurrentPosition +1) === 120) {
+                    pacmanCurrentPosition = 105
+                }
+                break
+          }
+        gridCells[pacmanCurrentPosition].classList.add('pacmanClass')
+        dotsCollected()
+    }
+    
+    // SCORE COUNTING
+
+    function dotsCollected() {
+        if(gridCells[pacmanCurrentPosition].classList.contains('pac-dot')) {
+            score += 10
+            scoreBoard.innerText = score
+            gridCells[pacmanCurrentPosition].classList.remove('pac-dot')
+            console.log('score')
         }
-    })
+    }
+    
+
+// // * Add ghost1 to grid
+
+//     function addGhost1(position) {
+//         gridCells[position].classList.add('ghost1')
+//         console.log(position)
+//     }
+//     addGhost1(ghost1StartPosition) 
+
+// // * Add ghost2 to grid
+
+//     function addGhost2(position) {
+//         gridCells[position].classList.add('ghost2')
+//         console.log(position)
+//     }
+//     addGhost2(ghost2StartPosition) 
+
+// // * Add ghost3 to grid
+
+//     function addGhost3(position) {
+//         gridCells[position].classList.add('ghost3')
+//         console.log(position)
+//     }
+//     addGhost3(ghost3StartPosition) 
+
+// // * Add ghost4 to grid
+
+//     function addGhost4(position) {
+//         gridCells[position].classList.add('ghost4')
+//         console.log(position)
+//     }
+//     addGhost4(ghost4StartPosition) 
+
+
+
+
+
+
+
+
+    document.addEventListener('keydown', movePacman) 
 
 }
 
