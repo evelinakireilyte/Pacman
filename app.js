352ed384f14c72a6cda06cd83013421caf323f51
Evalina Kireilyte <evalinakireilyte@michaels-mbp-2.lan>
2021-10-13 20:54:02 +0100

    stiling, audio, reset button

--- app.js
+++ app.js
@@ -28,12 +28,10 @@ function init() {
     let grid = document.querySelector('.grid')
     const gridCells = []
 
-    let pacmanCurrentPosition = 107 // use let to track where the pacman currently is (refers to an index)
+    let pacmanCurrentPosition = 106 
 
     let score = 0
-
     let lives = 3
-
 
     let scoreBoard = document.querySelector('.score-box')
     let livesBoard = document.querySelector('.lives-box')
@@ -52,8 +50,6 @@ function init() {
             livesBoard.innerText = lives
             scoreBoard.innerText = score
 
-         // * ADD LAYOUT TO THE GRID
-
             if (gridArray[i] === 0) {
                 gridCells[i].classList.add('pac-dot')
             } else if (gridArray[i] === 1) {
@@ -63,17 +59,47 @@ function init() {
             } else if (gridArray[i] === 3) {
                 gridCells[i].classList.add('fruit')
             }
-        }
-    }    
+        }    
+    }
     createGrid()
+
+//  * GAME RESET
+
+    function resetGrid() {
+        for (let i = 0; i < cellCount; i++) {
+            const cell = document.createElement('div')
+            cell.innerText = i 
+
+            livesBoard.innerText = 3
+            scoreBoard.innerText = 0
+
+            if (gridArray[i] === 0) {
+                gridCells[i].classList.add('pac-dot')
+            } else if (gridArray[i] === 3) {
+                gridCells[i].classList.add('fruit')
+            }
+        }   
+
+        gridCells[ghost.currentGhostPosition].classList.remove('ghost')  
+    }
+
 
 // * ADD PACMAN TO THE GRID
 
     function addPacman() {
         gridCells[pacmanCurrentPosition].classList.add('pacmanClass')
-        console.log(pacmanCurrentPosition)
     }
     addPacman() // call the function to add the pacman at its starting position
+
+
+// * PLAY THE GAME  -  when start button is pressed
+
+    function playGame() {
+        ghosts.forEach(ghost => moveGhosts(ghost))
+        document.addEventListener('keydown', movePacman)
+        // mainAudio()
+    }
+
 
 // * MOVE PACMAN
 
@@ -85,19 +111,27 @@ function init() {
             case 'ArrowUp':
                 if (pacmanCurrentPosition - width >=0 && !gridCells[pacmanCurrentPosition-width].classList.contains('wall') && !gridCells[pacmanCurrentPosition-width].classList.contains('ghost-home')) {
                     pacmanCurrentPosition -= width
+                    gridCells[pacmanCurrentPosition].classList.add('pacmanClass')
+                    rotatePacUp()
                 }
                 break
             case 'ArrowLeft':
                 if (pacmanCurrentPosition % width !==0 && !gridCells[pacmanCurrentPosition-1].classList.contains('wall') && !gridCells[pacmanCurrentPosition-1].classList.contains('ghost-home')) {
                     pacmanCurrentPosition -= 1
+                    gridCells[pacmanCurrentPosition].classList.add('pacmanClass')
+                    rotatePacLeft()
                 }    
                 if ((pacmanCurrentPosition -1) === 104) {
                     pacmanCurrentPosition = 119
+                    gridCells[pacmanCurrentPosition].classList.add('pacmanClass')
+                    rotatePacLeft()
                 }
                 break
             case 'ArrowDown':
                 if (pacmanCurrentPosition + width < width * width && !gridCells[pacmanCurrentPosition+width].classList.contains('wall') && !gridCells[pacmanCurrentPosition+width].classList.contains('ghost-home')) {
                     pacmanCurrentPosition += width
+                    gridCells[pacmanCurrentPosition].classList.add('pacmanClass')
+                    rotatePacDown()
                 }
                 break
             case 'ArrowRight':
@@ -112,7 +146,8 @@ function init() {
         gridCells[pacmanCurrentPosition].classList.add('pacmanClass')
         dotsCollected()
         fruitCollected()
-        gameOverPac()
+        livesLostP()
+        playerWins()
     }
     
 // * DOT SCORE COUNTING
@@ -145,7 +180,7 @@ function init() {
             this.currentGhostPosition = ghostPosition
             this.ghostTimer = NaN
         }
-        gameOverGhost() {
+        livesLostG() {
             if (gridCells[ghost.currentGhostPosition].classList.contains('pacmanClass') && lives >= 1) {
                 lives -= 1
                 livesBoard.innerText = lives
@@ -171,10 +206,8 @@ function init() {
         gridCells[ghost.currentGhostPosition].classList.add('ghost')  
     })
 
+// * MOVE GHOSTS
 
-// * MOVE GHOSTS (setInterval)
-
-    ghosts.forEach(ghost => moveGhosts(ghost))
 
     function moveGhosts(ghost) {
         const directions = [+1, -1, +width, -width]
@@ -189,14 +222,14 @@ function init() {
             } else {
                 ghostDirection = directions[Math.floor(Math.random() * directions.length)]
             }
-            gameOverPac()
+            livesLostP()
         }, ghost.ghostSpeed);
 
     }
 
-// * LIVE COUNT (if pacman walks into ghost ) 
+// * LIVES COUNTER (if pacman walks into ghost ) 
 
-    function gameOverPac() {
+    function livesLostP() {
         if (gridCells[pacmanCurrentPosition].classList.contains('ghost') && lives >= 1) {
             lives -= 1
             livesBoard.innerText = lives
@@ -205,15 +238,60 @@ function init() {
         } else if (gridCells[pacmanCurrentPosition].classList.contains('ghost') && lives == 0) {
             livesBoard.innerHTML = 0
             return window.alert('YOU HAVE LOST. Press START to try again.')
-        }    
+        }   
     }
     
+// * PACMAN ROTATION FUNCTIONS:
 
+    function rotatePacUp() {
+        let pacImg = document.querySelector('.pacmanClass')
+        pacImg.setAttribute('style','transform:rotate(270deg)')
+    }
 
+    function rotatePacLeft() {
+        let pacImg = document.querySelector('.pacmanClass')
+        pacImg.setAttribute('style','transform:rotate(180deg)')
+    }
 
-    document.addEventListener('keydown', movePacman) 
+    function rotatePacDown() {
+        let pacImg = document.querySelector('.pacmanClass')
+        pacImg.setAttribute('style','transform:rotate(90deg)')
+    }
+    
+    // PLAYER WINS THE GAME
+
+    function playerWins() {
+        if (score ===1210) {
+            return window.alert('CONGRATULATIONS, YOU WON THE GAME!')
+        }
+    }        
+
+    // GAME RESET FUNCTIONS:
+
+    function resetGame() {
+        gridCells[pacmanCurrentPosition].classList.remove('pacmanClass')
+        pacmanCurrentPosition = 106
+        gridCells[pacmanCurrentPosition].classList.add('pacmanClass')
+
+        scoreBoard.innerText = 0
+        livesBoard.innerText = 3
+
+        resetGrid()
+    }
+    
+//  * AUDIO PLAY FUNCTION
+
+    function mainAudio() {
+        const audio1 = document.getElementById('myAudio')
+        audio1.volume = 0.05
+        audio1.play()
+    }
+
+    startButton.addEventListener('click', playGame)
+    restartButton.addEventListener('click', resetGame)
 
 }
+
 
 
 window.addEventListener('DOMContentLoaded', init)
